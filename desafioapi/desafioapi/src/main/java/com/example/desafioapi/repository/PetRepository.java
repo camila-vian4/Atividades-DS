b50f6827901511aac0aa09291f651d3220f94026
Camila Viana <camila.v.lopes@ba.estudante.senai.br>
2026-04-01 21:41:10 -0300

    Delete desafioapi/desafioapi directory

--- desafioapi/desafioapi/src/main/java/com/example/desafioapi/repository/PetRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.desafioapi.repository;
-
-import com.example.desafioapi.model.PetModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PetRepository extends JpaRepository<PetModel, Long> {
-}
